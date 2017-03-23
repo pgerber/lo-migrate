10fcc5ab3e6f54e8782d99572cf8e199eaa51cba
Peter Gerber <peter@arbitrary.ch>
2017-03-23 21:06:29 +0100

    rework committing of sha2 hashes

--- src/thread/commit.rs
+++ src/thread/commit.rs
@@ -26,23 +26,21 @@ impl<'a> Committer<'a> {
     }
 
     pub fn start_worker(&self, rx: Arc<Receiver<Lo>>, chunk_size: usize) -> Result<()> {
-        let mut lo_chunk = Vec::with_capacity(chunk_size);
+        let mut lo_chunk: Vec<_> =
+            (0..chunk_size).map(|_| Lo::new(vec![], 0, i64::min_value(), "".to_string())).collect();
 
         loop {
-            Self::receive_next_chunk(&rx, &mut lo_chunk, chunk_size);
+            let size = Self::receive_next_chunk(&rx, &mut lo_chunk[..]);
 
             // commit sha2 hash to DB
-            commit::commit(self.conn, &lo_chunk)?;
+            commit::commit(self.conn, &lo_chunk[..size])?;
 
             // increase counter of committed `Lo`s
-            self.stats.lo_committed.fetch_add(lo_chunk.len() as u64, Ordering::Relaxed);
+            self.stats.lo_committed.fetch_add(size as u64, Ordering::Relaxed);
 
-            if lo_chunk.len() < chunk_size {
+            if size < chunk_size {
                 break; // sender hung up queue
             }
-
-            // clear committed `Lo`s
-            lo_chunk.clear();
 
             // thread cancellation point
             self.stats.cancellation_point()?;
@@ -53,14 +51,14 @@ impl<'a> Committer<'a> {
         Ok(())
     }
 
-    fn receive_next_chunk(rx: &Receiver<Lo>, lo_chunk: &mut Vec<Lo>, chunk_size: usize) {
-        while let Ok(lo) = rx.recv() {
-            lo_chunk.push(lo);
-
-            if lo_chunk.len() >= chunk_size {
-                break;
+    fn receive_next_chunk(rx: &Receiver<Lo>, lo_chunk: &mut [Lo]) -> usize {
+        for (i, mut item) in lo_chunk.iter_mut().enumerate() {
+            match rx.recv() {
+                Ok(lo) => *item = lo,
+                _ => return i,
             }
         }
+        lo_chunk.len()
     }
 }
 
@@ -75,17 +73,16 @@ mod tests {
         let (tx, rx) = two_lock_queue::channel(5);
         thread::spawn(move || { send_objects(tx, 12); });
 
-        let mut buffer = Vec::with_capacity(12);
-        Committer::receive_next_chunk(&rx, &mut buffer, 10);
-        assert!(buffer.iter().map(|i| i.size()).eq(0..10));
+        let mut buffer: Vec<_> =
+            (0..10).map(|_| Lo::new(vec![], 1000, 1000, "".to_string())).collect();
+        let size = Committer::receive_next_chunk(&rx, &mut buffer[..]);
+        assert!(buffer[..size].iter().map(|i| i.size()).eq(0..10));
 
-        buffer.clear();
-        Committer::receive_next_chunk(&rx, &mut buffer, 10);
-        assert!(buffer.iter().map(|i| i.size()).eq(10..12));
+        let size = Committer::receive_next_chunk(&rx, &mut buffer[..]);
+        assert!(buffer[..size].iter().map(|i| i.size()).eq(10..12));
 
-        buffer.clear();
-        Committer::receive_next_chunk(&rx, &mut buffer, 10);
-        assert!(buffer.is_empty());
+        let count = Committer::receive_next_chunk(&rx, &mut buffer);
+        assert_eq!(count, 0);
     }
 
     fn send_objects(tx: Sender<Lo>, count: i64) {
