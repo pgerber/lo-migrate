a91b79b9b5114060585e2e3f2739f2ab115883dd
Peter Gerber <peter@arbitrary.ch>
2017-03-23 15:01:11 +0100

    improve tests for monitor thread
    Ensure thread terminates only once all queue are closed.

--- tests/monitor.rs
+++ tests/monitor.rs
@@ -8,13 +8,19 @@ use std::time::{Duration, Instant};
 use std::thread::{self, JoinHandle};
 use queue::Receiver;
 
+type Receivers = (Option<Arc<Receiver<Lo>>>, Option<Arc<Receiver<Lo>>>, Option<Arc<Receiver<Lo>>>);
+
 #[test]
 fn queues_hang_up() {
-    let (handle, rx, stats) = start_monitor(86400);
+    let (handle, mut rx, stats) = start_monitor(86400);
     let start = Instant::now();
-    thread::spawn(|| {
-        thread::sleep(Duration::from_secs(3));
-        drop(rx);
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(1));
+        drop(rx.0.take()); // hang up first queue
+        thread::sleep(Duration::from_secs(1));
+        drop(rx.1.take()); // hang up second queue
+        thread::sleep(Duration::from_secs(1));
+        // hang up third queue
     });
     handle.join().unwrap();
     assert!(start.elapsed() >= Duration::from_secs(3));
@@ -35,24 +41,30 @@ fn cancelled() {
     assert!(stats.is_cancelled());
 }
 
-fn start_monitor(secs: u64) -> (JoinHandle<()>, Arc<Receiver<Lo>>, ThreadStat) {
-    let (_, rx) = queue::unbounded();
-    let rx = Arc::new(rx);
+fn start_monitor(secs: u64) -> (JoinHandle<()>, Receivers, ThreadStat) {
+    let (_, rx1) = queue::unbounded();
+    let rx1 = Arc::new(rx1);
+    let (_, rx2) = queue::unbounded();
+    let rx2 = Arc::new(rx2);
+    let (_, rx3) = queue::unbounded();
+    let rx3 = Arc::new(rx3);
     let stats = ThreadStat::new();
 
-    let rx_weak = Arc::downgrade(&rx);
+    let rx1_weak = Arc::downgrade(&rx1);
+    let rx2_weak = Arc::downgrade(&rx2);
+    let rx3_weak = Arc::downgrade(&rx3);
     let stats_thread = stats.clone();
     let handle = thread::spawn(move || {
         let monitor = Monitor {
             stats: &stats_thread,
-            receive_queue: rx_weak.clone(),
+            receive_queue: rx1_weak,
             receive_queue_size: 10,
-            store_queue: rx_weak.clone(),
+            store_queue: rx2_weak,
             store_queue_size: 15,
-            commit_queue: rx_weak,
+            commit_queue: rx3_weak,
             commit_queue_size: 20,
         };
         monitor.start_worker(Duration::from_secs(secs));
     });
-    (handle, rx, stats)
+    (handle, (Some(rx1), Some(rx2), Some(rx3)), stats)
 }
