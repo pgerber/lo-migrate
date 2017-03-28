pgerber/lo-migrate

commit c43bb994ebdd1c6eea321fc97c0d349ddb28f886

create temporary files securely
The mktemp crate does not create the files securely. Created files
are world readable and the file creation itself doesn't appear to
be secure either. Switching to mkstemp-rs.

src/receive.rs
@@ -2,15 +2,14 @@
 
 use error::Result;
 use lo::{Data, Lo};
-use mktemp::Temp;
+use mkstemp::TempFile;
 use postgres::Connection;
 use postgres_large_object::{LargeObjectTransactionExt, Mode};
 use digest::Digest;
 #[cfg(feature = "try_from")]
 use std::convert::TryInto;
-use std::fs;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 
 impl Lo {
     /// Retrieve Large Object data
@@ -55,9 +54,9 @@ impl Lo {
             (Data::Vector(data), size)
         } else {
             // keep binary data in temporary file
-            let temp_file = Temp::new_file()?;
-            let mut file = fs::File::create(&temp_file)?;
-            let size = io::copy(&mut sha2_reader, &mut file)?;
+            let mut temp_file = TempFile::new("lo_migrate.XXXXXX", true)?;
+            let size = io::copy(&mut sha2_reader, &mut temp_file)?;
+            temp_file.flush()?;
             (Data::File(temp_file), size)
         };
 

Cargo.toml
@@ -16,7 +16,7 @@ aws-sdk-rust = "0.1"
 rustc-serialize = "0.3"
 two-lock-queue = "0.1"
 clippy = { version = "*", optional = true }
-mktemp = "0.3"
+mkstemp-rs = "0.0"
 memmap = "0.5"
 hyper = "0.10"
 base64 = "0.4"

src/lib.rs
@@ -52,7 +52,7 @@ extern crate aws_sdk_rust;
 extern crate chrono;
 extern crate digest;
 extern crate fallible_iterator;
-extern crate mktemp;
+extern crate mkstemp;
 extern crate postgres;
 extern crate postgres_large_object;
 extern crate rustc_serialize as serialize;

src/lo.rs
@@ -4,7 +4,7 @@ use postgres::types::Oid;
 use std::fmt;
 use std::mem;
 use serialize::hex::ToHex;
-use mktemp::Temp;
+use mkstemp::TempFile;
 use base64;
 
 /// Large Object Stored in memory or in a temporary file
@@ -13,7 +13,7 @@ pub enum Data {
     Vector(Vec<u8>),
 
     /// Large Object stored in a temporary file
-    File(Temp),
+    File(TempFile),
 
     /// Large Object not yet or no longer available
     ///
@@ -25,7 +25,7 @@ impl fmt::Debug for Data {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Data::Vector(ref v) => write!(fmt, "Vector(0x{}...)", &v[..4].to_hex()),
-            Data::File(ref f) => write!(fmt, "File({:?})", f.as_ref()),
+            Data::File(ref f) => write!(fmt, "File({:?})", f.path()),
             Data::None => write!(fmt, "None"),
         }
     }

src/store.rs
@@ -22,7 +22,7 @@ impl Lo {
                 // TODO:
                 // Our AWS S3 library takes `&[u8]` as object content, it really should use trait
                 // `Read`. To avoid excessive memory use, the file is mapped into memory for now.
-                let mapped_file = Mmap::open_path(temp.as_ref(), Protection::Read)?;
+                let mapped_file = Mmap::open_path(temp.path(), Protection::Read)?;
 
                 let data = unsafe {
                     // This is considered unsafe because the mapped file may be altered

commit 34494a694a2727f72b5277109b6c24dc17efa78c

create temporary file in OS specific temp directory
This fixes a regression introduced by c43bb994ebdd1c6eea.

src/receive.rs
@@ -8,6 +8,7 @@ use postgres_large_object::{LargeObjectTransactionExt, Mode};
 use digest::Digest;
 #[cfg(feature = "try_from")]
 use std::convert::TryInto;
+use std::env;
 use std::io;
 use std::io::{Read, Write};
 
@@ -54,7 +55,10 @@ impl Lo {
             (Data::Vector(data), size)
         } else {
             // keep binary data in temporary file
-            let mut temp_file = TempFile::new("lo_migrate.XXXXXX", true)?;
+            let mut temp_path = env::temp_dir();
+            temp_path.push("lo_migrate.XXXXXX");
+            let mut temp_file =
+                TempFile::new(temp_path.to_str().expect("tempdir not a UTF-8 path"), true)?;
             let size = io::copy(&mut sha2_reader, &mut temp_file)?;
             temp_file.flush()?;
             (Data::File(temp_file), size)